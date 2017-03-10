843ea9cc43cb86aeec2c068935a6c8cc75e30461
Andrea Saltarello <andysal@gmail.com>
2017-03-10 18:29:14 +0100

    1.1.1

---
+++ src/MementoFX.Messaging.Rebus.Tests/RebusEventDispatcherFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpTestsEx;
+using Rebus.Config;
+using Rebus.Sagas;
+using Rebus.Handlers;
+using System.Threading.Tasks;
+using Rebus.Bus;
+using Moq;
+
+namespace Memento.Messaging.Rebus.Tests
+{
+    [TestClass]
+    public class RebusEventDispatcherFixture
+    {
+        [TestMethod]
+        public void Ctor_should_throw_ArgumentNullException_on_null_bus_and_value_of_parameter_should_be_bus()
+        {
+            Executing.This(() => new RebusEventDispatcher(null))
+                .Should()
+                .Throw<ArgumentNullException>()
+                .And
+                .ValueOf
+                .ParamName
+                .Should()
+                .Be
+                .EqualTo("bus");
+        }
+
+    }
+}
---
+++ src/MementoFX.Messaging.Rebus/RebusEventDispatcher.cs
@@ -0,0 +1,43 @@
+﻿using Rebus.Bus;
+using System;
+
+namespace Memento.Messaging.Rebus
+{
+    /// <summary>
+    /// Provides the implementation a Memento event dispatcher
+    /// based upon Rebus
+    /// </summary>
+    public class RebusEventDispatcher : IEventDispatcher
+    {
+        /// <summary>
+        /// Gets or sets the reference to the bus instance
+        /// used by the dispatcher
+        /// </summary>
+        protected IBus Bus { get; private set; }
+
+        /// <summary>
+        /// Creates an instance of the event dispatcher
+        /// </summary>
+        /// <param name="bus"></param>
+        public RebusEventDispatcher(IBus bus)
+        {
+            if (bus == null)
+                throw new ArgumentNullException("bus");
+
+            Bus = bus;
+        }
+
+        /// <summary>
+        /// Dispatches an event
+        /// </summary>
+        /// <typeparam name="T">The type of the event</typeparam>
+        /// <param name="event">The event to dispatch</param>
+        public void Dispatch<T>(T @event) where T : DomainEvent
+        {
+            if (@event == null)
+                throw new ArgumentNullException(nameof(@event));
+
+            Bus.Publish(@event).Wait();
+        }
+    }
+}
